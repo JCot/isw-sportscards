41ec41c8f80916df600237dbe89db6a5bd1a5534
Justin Cotner <justin.cotner@hudl.com>
2015-07-29 09:25:24 -0500

    Fresh project

---
+++ SportsCards/SportsCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportsCards
+//
+//  Created by Justin Cotner on 7/29/15.
+//  Copyright (c) 2015 Justin Cotner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
